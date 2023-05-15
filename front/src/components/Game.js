aec8646242a0adb9dfa6e1c7f07a7571e0ba277e
bojinovic <milos.bojinovic@mvpworkshop.co>
2023-05-15 21:41:18 +0200

    Front - making progress on game play

--- front/src/components/Game.js
+++ front/src/components/Game.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Sketch from "react-p5";
 import * as common from "../utils/common";
-import { Player } from "../utils/game";
+import { Field, Player } from "../utils/game";
 
 // import { init } from "../utils/game";
 import "../style/css/Game.css";
@@ -11,7 +11,9 @@ import { useEffect } from "react";
 let y = 0;
 let direction = "^";
 let ellipse;
-let player;
+let field;
+let players;
+let flag = false;
 
 const Game = () => {
   const setup = (p5, parentRef) => {
@@ -19,19 +21,38 @@ const Game = () => {
       common.GAME_SCENE_DIMENSIONS.width,
       common.GAME_SCENE_DIMENSIONS.height
     ).parent(parentRef);
-    player = new Player(p5, 1, 0);
-    console.log({ player });
+
+    field = new Field(
+      p5,
+      common.GAME_SCENE_DIMENSIONS.width,
+      common.GAME_SCENE_DIMENSIONS.height
+    );
+    players = [
+      new Player(p5, 50, 100, 5, "rgba(255, 0, 0, 0.5)"),
+      new Player(p5, 50, 100, 1, "rgba(255, 0, 0, 0.5)"),
+      new Player(p5, 50, 100, 2, "rgba(255, 0, 0, 0.5)"),
+      new Player(p5, 50, 100, 3, "rgba(255, 0, 0, 0.5)"),
+      new Player(p5, 50, 100, 4, "rgba(255, 0, 0, 0.5)"),
+      new Player(p5, 150, 150, 1, "rgba(0, 0, 255, 0.5)"),
+      new Player(p5, 250, 350, 2, "rgba(0, 0, 255, 0.5)"),
+      new Player(p5, 450, 150, 3, "rgba(0, 0, 255, 0.5)"),
+      new Player(p5, 550, 50, 4, "rgba(0, 0, 255, 0.5)"),
+      new Player(p5, 110, 850, 5, "rgba(0, 0, 255, 0.5)"),
+    ];
   };
-  const draw = (p5) => {
-    console.log({ player });
-    p5.background(255, 255, 255);
-    {
-      y = (y + 1) % 100;
+  const draw = async (p5) => {
+    p5.background("rgba(244, 244, 244, 1)");
 
-      player.move(0, y);
+    field.animate();
 
-      player.animate();
+    if (players[0].moving == false) {
+      players.forEach((p, k) => {
+        p.move(Math.random() * 800, Math.random() * 500);
+      });
     }
+    players.forEach((p) => {
+      p.animate();
+    });
   };
 
   useEffect(() => {
--- front/src/utils/game.js
+++ front/src/utils/game.js
@@ -2,29 +2,100 @@ import * as common from "./common";
 
 const META = {};
 
+class Field {
+  constructor(p5, w, h) {
+    this.p5 = p5;
+
+    this.w = w;
+    this.h = h;
+  }
+
+  animate() {
+    const GOAL_H = 130;
+    const GOAL_W = 60;
+    this.p5.stroke(0, 0, 0);
+    this.p5.line(this.w / 2, 0, this.w / 2, this.h);
+    this.p5.fill(255, 255, 255, 0);
+    this.p5.ellipse(this.w / 2, this.h / 2, 200);
+    this.p5.rect(0, this.h / 2 - GOAL_H / 2, GOAL_W, GOAL_H);
+    this.p5.rect(this.w - GOAL_W, this.h / 2 - GOAL_H / 2, GOAL_W, GOAL_H);
+    this.p5.rect(0, 0, this.w, this.h);
+  }
+}
+
 class Player {
-  constructor(p5, _x, _y) {
+  constructor(p5, _x, _y, number, color) {
+    this.p5 = p5;
+
     this.x = _x;
     this.y = _y;
+    this.number = number;
+    this.color = color;
 
     this.delta = [0, 0];
-    this.steps = 20;
-    this.p5 = p5;
+    this.steps = 50;
+    this.currStep = 0;
+
+    this.moving = false;
   }
 
   move(_x, _y) {
     this.delta = [(_x - this.x) / this.steps, (_y - this.y) / this.steps];
-    if (isNaN(this.delta[0])) {
-      throw Error("err");
-    }
+    this.nextX = _x;
+    this.nextY = _y;
+    this.currStep = 0;
+    this.moving = true;
   }
 
   animate() {
-    this.x += this.delta[0];
-    this.y += this.delta[1];
-    this.p5.fill(127, 21);
-    this.p5.ellipse(this.x, this.y, 50);
+    this.currStep = (this.currStep + 1) % this.steps;
+
+    if (this.currStep != 0) {
+      this.x += this.delta[0];
+      this.y += this.delta[1];
+    } else {
+      this.x = this.nextX;
+      this.y = this.nextY;
+      this.delta = [0, 0];
+      this.moving = false;
+    }
+
+    this.p5.fill(this.color);
+    this.p5.ellipse(this.x, this.y, 10);
+    this.drawArrow();
+    this.drawRectInfo();
+    this.drawNumber();
+  }
+
+  drawArrow() {
+    this.p5.stroke(0, 0, 0);
+    this.p5.fill(0, 0, 0, 0.2);
+    this.p5.line(this.x, this.y, this.nextX, this.nextY);
+    this.p5.ellipse(this.nextX, this.nextY, 2);
+  }
+
+  drawRectInfo() {
+    const RECT_H = 30;
+    const RECT_W = 40;
+    this.p5.stroke(this.color);
+    this.p5.fill("rgba(255,255,255,0.9)");
+    this.p5.rect(this.x - RECT_W / 2, this.y - 20 - RECT_H, RECT_W, RECT_H);
+    this.p5.triangle(
+      this.x,
+      this.y - 10,
+      this.x - 17,
+      this.y - 15,
+      this.x + 17,
+      this.y - 15
+    );
+    this.p5.stroke(0, 0, 0);
+  }
+
+  drawNumber() {
+    this.p5.textSize(32);
+    this.p5.fill(this.color);
+    this.p5.text(`${this.number}`, this.x - 8, this.y - 23);
   }
 }
 
-export { Player };
+export { Field, Player };
