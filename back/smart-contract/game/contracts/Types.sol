2c99d2ce178a4cf7e50d6e5f4bed080b8747b1c0
bojinovic <milos.bojinovic@mvpworkshop.co>
2023-05-24 20:25:38 +0200

    Back - Smart Contracts - defined main types that will be used

--- back/smart-contract/game/contracts/Types.sol
+++ back/smart-contract/game/contracts/Types.sol
@@ -2,8 +2,18 @@ pragma solidity ^0.8.18;
 
 contract Types {
 
-
-    address public ticker;
+    enum MATCH_STAGE {
+        P1_CREATED_THE_MATCH,
+        P2_JOINED_THE_MATCH,
+        RANDOM_SEED_FETCHED,
+        RANDOM_SEED_RECEIVED,
+        COMMITMENTS_FETCHED,
+        COMMITMENTS_RECEIVED,
+        REVEALS_FETCHED,
+        REVEAL_RECEIVED,
+        STATE_UPDATE_PERFORMED,
+        MATCH_ENDED
+    }
 
     struct PlayerStats {
         uint speed;
@@ -11,66 +21,49 @@ contract Types {
         uint stamina;
     }
 
-    enum MATCH_STATE {DUMMY, INIT_PHASE, COMMITMENT_PHASE, REVEAL_PHASE, STATE_UPDATE_PHASE}
-
     struct MatchInfo {
         uint seed;
 
-        PlayerStats[] team1_playerStats;
-        PlayerStats[] team2_playerStats;
+        address commitmentFunctionConsumer;
+        address revealFunctionConsumer;
 
-        address team1_commitmentChainlinkFunctionConsumer;
-        address team2_commitmentChainlinkFunctionConsumer;
-
-        address team1_revealChainlinkFunctionConsumer;
-        address team2_revealChainlinkFunctionConsumer;
-
-        bool initFinished;
+        MATCH_STAGE stage;
     }
 
-    struct CommitmentInfo {
-        bytes data;
+    struct TeamState {
+        uint[] xPos;
+        uint[] yPos;
+
+        bool hasTheBall;
+        uint playerIdWithTheBall;
     }
 
-    struct RevealInfo {
+    struct TeamMove {
+        bytes commitment;
+        bytes reveal;
 
-        bytes data;
-        
-        uint seed;
-        uint[] team_x_positions;
-        uint[] team_y_positions;
-        
-        bool wants_to_pass;
-        uint receiving_player_id;
+        uint[] xPos;
+        uint[] yPos;
 
-        bool wants_to_shoot;
+        bool wantToPass;
+        uint receivingPlayerId;
+
+        bool wantToShoot;
     }
 
-    struct StateInfo {
-        PlayerStats[] team1_playerStats;
-        PlayerStats[] team2_playerStats;
+    struct ProgressionState {
+        bool ballWasWon;
+        uint ballWasWonByTeam;
 
-        uint[] team1_x_positions;
-        uint[] team2_x_positions;
-        uint[] team1_y_positions;
-        uint[] team2_y_positions;
+        bool interceptionOccured;
+        uint interceptionAchievedByTeam;
+        uint[] ballXPos;
+        uint[] ballYPos;
 
-        uint team_with_the_ball;
-        uint player_id_with_the_ball;
+        bool goalWasScored;
+        uint goalWasScoredByTeam;
 
-        bool pass;
-        uint receiving_player_id;
-        bool interceptionOcurred;
-        uint[] pass_ball_x_positions;
-        uint[] pass_ball_y_positions;
-        
-        bool shoot;
+        TeamState[] teamState;
     }
 
-    struct MoveInfo {
-        CommitmentInfo[] commitments;
-        RevealInfo[] reveals;
-
-        StateInfo state;
-    }
 }
