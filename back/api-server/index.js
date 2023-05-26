74283f309bcf8652a90482b0fe247281fd40ac88
bojinovic <milos.bojinovic@mvpworkshop.co>
2023-05-26 17:49:55 +0200

    Back - API server - experimenting with terminal display

--- back/api-server/index.js
+++ back/api-server/index.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const ethers = require("ethers");
+import express from "express";
+import { ethers } from "ethers";
+import chalk from "chalk";
+import terminalImage from "terminal-image";
+import got from "got";
+import figlet from "figlet";
 
 const app = express();
 const abi = ethers.utils.defaultAbiCoder;
 
 const port = 3000;
 
-const RESPONSE_LENGTH = 10;
+const RESPONSE_LENGTH = 6;
 
 const randomData = () => {
   const arr = [];
@@ -17,11 +21,11 @@ const randomData = () => {
 };
 
 let currData = randomData();
-let currEncodedData = abi.encode(["uint8[10]"], [currData]);
+let currEncodedData = abi.encode(["uint256[6]"], [currData]);
 
 app.get("/p1/generate-random-data", async (req, res) => {
   currData = randomData();
-  currEncodedData = abi.encode(["uint8[10]"], [currData]);
+  currEncodedData = abi.encode(["uint256[6]"], [currData]);
   return res.json({ data: currData, encoded: currEncodedData });
 });
 app.get("/p1/get-commitment", async (req, res) => {
@@ -36,6 +40,81 @@ app.get("/p1/get-reveal", async (req, res) => {
   return res.json({ data: currEncodedData });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, async () => {
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  // const img1 = await got(
+  //   "https://freeiconshop.com/wp-content/uploads/edd/key-flat-128x128.png"
+  // ).buffer();
+
+  // const img2 = await got(
+  //   "https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/128x128/ok.png"
+  // ).buffer();
+  const fonts = figlet.fontsSync();
+  // for (const font of fonts.slice(100)) {
+  //   // console.clear();
+  //   // let body = img1;
+  //   // console.log(await terminalImage.buffer(body, { height: 5 }));
+
+  //
+
+  //   // await delay(2000);
+  //   // console.clear();
+
+  //   // body = img2;
+
+  //   // console.log(await terminalImage.buffer(body, { height: 5 }));
+  //   console.log(
+  //     chalk.red(
+  //       figlet.textSync("AI.v1", {
+  //         font,
+  //         width: 100,
+  //         whitespaceBreak: true,
+  //       })
+  //     )
+  //   );
+  //   console.log({ font });
+  //   //ANSI Shadow | 'Big Chief' | Big Money | 'DOS Rebel'
+  //   // await delay(2000);
+  //   // console.clear();
+  // }
+
+  const strs = [" Calmdogs"];
+
+  let logs = [];
+
+  while (true) {
+    for (const str of strs) {
+      console.clear();
+
+      console.log("\n");
+
+      console.log(
+        chalk.blue(
+          figlet.textSync(str, {
+            font: "DOS Rebel",
+            width: 100,
+            whitespaceBreak: true,
+          })
+        )
+      );
+
+      for (let i = 0; i < logs.length && i < 15; ++i) {
+        console.log(logs[Math.min(logs.length - 1, 14) - i]);
+      }
+
+      await delay(2000);
+
+      logs = [`STR: ${Math.floor(Math.random() * 23456781123)}`, ...logs];
+      if (Math.random() > 0.3) {
+        logs = [
+          `Chosen move: [12, 341, 5116, 2346 , 24124 , 67,356 , 1234 12]`,
+          ...logs,
+        ];
+      }
+
+      if (Math.random() > 0.3) {
+        logs = [`Match state has been updated and observed`, ...logs];
+      }
+    }
+  }
 });
